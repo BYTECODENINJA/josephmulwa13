bc2661e707dd4a810c00eb584b77c3eae3ee2135
Joseph Mulwa <josephmulwa808@outlook.com>
2025-12-14 10:50:28 +0300

    Updates

--- _next/static/i39-0-44wixSeoc2y5QmV/_buildManifest.js
+++ _next/static/i39-0-44wixSeoc2y5QmV/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/josephmulwa13//_next/:path+",
+        "destination": "/josephmulwa13/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
